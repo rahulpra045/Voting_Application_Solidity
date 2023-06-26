111195ba476dc887b16418cee37b2984368817a3
Rahul Prasad <98214910+rahulpra045@users.noreply.github.com>
2023-06-26 15:49:06 +0530

    Add files via upload

---
+++ src/Components/Connected.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Connected = (props) => {
+    return (
+        <div className="connected-container">
+            <h1 className="connected-header">You are Connected to Metamask</h1>
+            <p className="connected-account">Metamask Account : {props.account}</p>
+            <p className="connected-account">Remaining Time : {props.remainingTime}</p>
+
+            {props.showButton ? (
+                <p className="connected-account">You have already Voted</p>
+            ) : (
+                    
+            <div>
+                <input type="number" placeholder="Enter Candidate Index" value={props.number} onChange={props.handleNumberChange}></input>
+                <button className="login-button" onClick={props.voteFunction}>Vote</button>
+            </div>
+            )}
+
+            <table id="myTable" className="candidates-table">
+                <thead>
+                <tr>
+                    <th>Index</th>
+                    <th>Candidate name</th>
+                    <th>Candidate votes</th>
+                </tr>
+                </thead>
+                <tbody>
+                {props.candidates.map((candidate, index) => (
+                    <tr key={index}>
+                    <td>{candidate.index}</td>
+                    <td>{candidate.name}</td>
+                    <td>{candidate.voteCount}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            
+
+        </div>
+    )
+}
+
+export default Connected;
